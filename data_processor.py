d57337abd7732e9d89c47fa95f86f9e9f99172e3
justinliu412 <justinliu412@hotmail.com>
2019-12-11 20:24:14 -0800

    grouped work in folders

--- data_processor.py
+++ data_processor.py
@@ -3,11 +3,17 @@ import os, sys
 import pdb
 import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_files
 
 TRAIN_PATH = './aclImdb/train/'
 VAL_PATH = './aclImdb/test/'
 
-TRAIN_LIST = os.listdir(TRAIN_PATH)
-VAL_LIST = os.listdir(VAL_PATH)
+# TRAIN_LIST = os.listdir(TRAIN_PATH)
+# VAL_LIST = os.listdir(VAL_PATH)
 
-print(TRAIN_LIST)
+# print(TRAIN_LIST)
+
+reviews_train = load_files(TRAIN_PATH)
+data_train, targets_train = reviews_train.data, reviews_train.target
+pdb.set_trace()
+
